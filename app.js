e9ebff487ccb97a7be6e7d1a735e019493efd37c
Bert Witzel <bwitzel82@gmail.com>
2021-06-19 13:10:09 -0400

    update app.js and pug files
    get home and about page up and running

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const data = require('./data.json');
+const projects = data.projects;
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', { projects });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects/:id', (req, res) => {
+
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
