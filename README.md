9051708b94cd13a8a0d891cec4be4851477fbe86
Bert Witzel <bwitzel82@gmail.com>
2021-06-21 09:11:57 -0400

    wrapping everything up
    delete code comments, add errors handlers, update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node.js-express-portfolio
  Portfolio website using static node.js and express
+
+In this project I created a portfolio of side projects using node.js, Express, and Pug. Several Pug files were included with this project, although I modified all of them to fulfill the requirements of the project, and I created `page-not-found.pug` and `error.pug` pages for user friendly error display. 
+
+I began the project by installing Express and Pug via node.js. I then created a `data.json` file where I included all of the information about the projects that I am displaying in this portfolio. All of my side projects are on live sites using GitHub pages. I took screenshots of all of the projects and reduced the file size with `squoosh.app` before uploading to my project. 
+
+In my `app.js` file I wrote all of the functionality to run the Express app and handle errors from scratch. I also modified the `styles.css` file to use different colors from the colors provided in the original project files. I got my current color scheme from https://coolors.co/. I also added an extra image and a gif for the error pages in order to provide some fun in the user experience. 
--- app.js
+++ app.js
@@ -4,22 +4,51 @@ const projects = data.projects;
 const app = express();
 
 app.use('/static', express.static('public'));
-
 app.set('view engine', 'pug');
 
+// render home page
 app.get('/', (req, res) => {
     res.render('index', { projects });
 });
 
+// render about page
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// render individual project pages
 app.get('/projects/:id', (req, res) => {
     const id = req.params.id;
     const project = projects[id];
     res.render('project', { project });
 });
+
+// test for global errors
+
+// app.use((req,res,next) => {
+//     console.log('hello');
+//     const err = new Error('oh no!');
+//     err.status = 500;
+//     next(err); 
+//   });
+
+// 404 errors
+app.use((req, res, next) => {
+    const err = new Error();
+    err.message = 'Page Not Found';
+    err.status = 404;
+    console.log(`Error ${err.status}: ${err.message}`);
+    res.render('page-not-found', { err });
+    next();
+});
+
+// global errors
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    console.log(`Error ${err.status}: ${err.message}`);
+    res.render('error', { err });
+  });
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
